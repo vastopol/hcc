0b07c866562a8def04dbc3f15087783bd249daf4
vastopol <classicunix@gmail.com>
2019-09-05 15:46:58 -0700

    Partially working. Assembles but wrong output

--- vm/program.py
+++ vm/program.py
@@ -77,7 +77,8 @@ class Program():
 
     # minimize the IR before translation
     def minimize_ir(self):
-        self.print_ir()
+        # self.print_ir()
+        # print('-'*40+'\n')
         self.minimize1() # assign assign
         self.minimize2() # read assign
         self.minimize3() # assign param
@@ -506,42 +507,60 @@ class Program():
 
     #----------------------------------------
 
+    # (depth)*4        for word sizes
+    # (depth+1)*4      for return address
+    # ((depth+1)+$#)*4 for stack params
+
     # translate IR to MIPS
     def translate_ir(self):
-        fname = str()
-        asm = list()
-
-        # translating
+        bin_ops = ["+", "-", "*", "/", "%", "==", "!=", ">", ">=", "<", "<=", "&&", "||"]
         for f in self.functions:
             fname = f
+            asm = list()
+            locals = dict()  # name and offset into the stack pointer
+            depth  = 1       # depth into stack
             for g in self.functions[f]:
                 # begin function
                 if g[0] == "func":
                     header  = g[1] + ":\n"
-                    # pop params into the s regs ??
                     push_ra = "sub $sp, $sp, 4\nsw $ra, 4($sp)\n"  # push $ra
                     asm.append(header + push_ra)
                 # end function
                 elif g[0] == "endfunc":
                     if fname == "main":  # main has no ret
+                        add_sp = "add $sp, $sp, 4\n"
+                        for i in range(depth-1):
+                            asm.append(add_sp)
                         rval   = "li $v0, 0\n"
                         pop_ra = "lw $ra, 4($sp)\nadd $sp, $sp, 4\n"   # pop $ra
                         ret    = "jr $ra\n"
                         asm.append(rval + pop_ra + ret)
                     self.assembly[fname] = asm
-                    fname = ""
-                    asm = list()
-                    continue
+                # locals
+                elif g[0] == "." or g[0] == ".[]":
+                    locals[g[1]] = depth
+                    depth += 1
+                    sub_sp  = "sub $sp, $sp, 4\n"
+                    asm.append(sub_sp)
                 # function call
                 elif g[0] == "call":
                     call = "jal " + g[1][:-1] + "\n"
-                    push_i  = "sub $sp, $sp, 4\nsw $v0, ($sp)\n"   # push $v0
+                    push_i  = "sw $v0, " + str( locals[g[2]]*4 ) + "($sp)\n"   # push $v0
                     asm.append(call + push_i)
+                # function return
                 elif g[0] == "ret":
-                    pop_i  = "lw $v0, ($sp)\naddiu $sp, $sp, 4\n"  # pop $v0
+                    pop_i = str()
+                    if str(g[1]).isdigit():  # immediate int
+                        pop_i  = "li $v0, " + g[1] + "\n"  # pop $v0
+                    else:
+                        pop_i  = "lw $v0, " + str( locals[g[1]]*4 ) + "($sp)\n"  # pop $v0
+                    add_i = "add $sp, $sp, 4\n"
                     pop_ra = "lw $ra, 4($sp)\nadd $sp, $sp, 4\n"   # pop $ra
                     ret    = "jr $ra\n"
-                    asm.append(pop_i + pop_ra + ret)
+                    asm.append(pop_i)
+                    for i in range(depth-1):  # stack vars
+                        asm.append(add_i)
+                    asm.append(pop_ra + ret)
                 # label
                 elif g[0] == ":":
                     label = g[1] + ":\n"
@@ -552,90 +571,92 @@ class Program():
                     asm.append(goto)
                 # branch
                 elif g[0] == "?:=":
-                    pop_i  = "lw $t0, ($sp)\naddiu $sp, $sp, 4\n"   # pop $t0
-                    branch = "bne $t0, $zero " + g[1][:-1] + "\n"
+                    t0 = locals[g[2]]*4
+                    pop_i  = "lw $t0, " + str(t0) + "($sp)\n"
+                    branch = "bne $t0, $zero, " + g[1][:-1] + "\n"
                     asm.append(pop_i + branch)
-                # add
-                elif g[0] == "+":
-                    pop_i_b = "lw $t2, ($sp)\naddiu $sp, $sp, 4\n"  # pop $t2
-                    pop_i_a = "lw $t1, ($sp)\naddiu $sp, $sp, 4\n"  # pop $t1
-                    add_i   = "add $t0, $t1, $t2\n"
-                    push_i  = "sub $sp, $sp, 4\nsw $t0, ($sp)\n"    # push $t0
-                    asm.append(pop_i_b + pop_i_a + add_i + push_i)
-                # sub
-                elif g[0] == "-":
-                    pop_i_b = "lw $t2, ($sp)\naddiu $sp, $sp, 4\n"  # pop $t2
-                    pop_i_a = "lw $t1, ($sp)\naddiu $sp, $sp, 4\n"  # pop $t1
-                    sub_i   = "sub $t0, $t1, $t2\n"
-                    push_i  = "sub $sp, $sp, 4\nsw $t0, ($sp)\n"    # push $t0
-                    asm.append(pop_i_b + pop_i_a + sub_i + push_i)
-                # mul
-                elif g[0] == "*":
-                    pass
-                # div
-                elif g[0] == "/":
-                    pass
-                # mod
-                elif g[0] == "%":
-                    pass
-                # eq
-                elif g[0] == "==":
-                    pop_i_b = "lw $t2, ($sp)\naddiu $sp, $sp, 4\n"  # pop $t2
-                    pop_i_a = "lw $t1, ($sp)\naddiu $sp, $sp, 4\n"  # pop $t1
-                    sub_i   = "seq $t0, $t1, $t2\n"
-                    push_i  = "sub $sp, $sp, 4\nsw $t0, ($sp)\n"    # push $t0
-                    asm.append(pop_i_b + pop_i_a + sub_i + push_i)
-                # neq
-                elif g[0] == "!=":
-                    pop_i_b = "lw $t2, ($sp)\naddiu $sp, $sp, 4\n"  # pop $t2
-                    pop_i_a = "lw $t1, ($sp)\naddiu $sp, $sp, 4\n"  # pop $t1
-                    sub_i   = "sne $t0, $t1, $t2\n"
-                    push_i  = "sub $sp, $sp, 4\nsw $t0, ($sp)\n"    # push $t0
-                    asm.append(pop_i_b + pop_i_a + sub_i + push_i)
-                # lt
-                elif g[0] == "<":
-                    pop_i_b = "lw $t2, ($sp)\naddiu $sp, $sp, 4\n"  # pop $t2
-                    pop_i_a = "lw $t1, ($sp)\naddiu $sp, $sp, 4\n"  # pop $t1
-                    sub_i   = "slt $t0, $t1, $t2\n"
-                    push_i  = "sub $sp, $sp, 4\nsw $t0, ($sp)\n"    # push $t0
-                    asm.append(pop_i_b + pop_i_a + sub_i + push_i)
-                # lte
-                elif g[0] == "<=":
-                    pop_i_b = "lw $t2, ($sp)\naddiu $sp, $sp, 4\n"  # pop $t2
-                    pop_i_a = "lw $t1, ($sp)\naddiu $sp, $sp, 4\n"  # pop $t1
-                    sub_i   = "sle $t0, $t1, $t2\n"
-                    push_i  = "sub $sp, $sp, 4\nsw $t0, ($sp)\n"    # push $t0
-                    asm.append(pop_i_b + pop_i_a + sub_i + push_i)
-                # gt
-                elif g[0] == ">":
-                    pop_i_b = "lw $t2, ($sp)\naddiu $sp, $sp, 4\n"  # pop $t2
-                    pop_i_a = "lw $t1, ($sp)\naddiu $sp, $sp, 4\n"  # pop $t1
-                    sub_i   = "sgt $t0, $t1, $t2\n"
-                    push_i  = "sub $sp, $sp, 4\nsw $t0, ($sp)\n"    # push $t0
-                    asm.append(pop_i_b + pop_i_a + sub_i + push_i)
-                # gte
-                elif g[0] == ">=":
-                    pop_i_b = "lw $t2, ($sp)\naddiu $sp, $sp, 4\n"  # pop $t2
-                    pop_i_a = "lw $t1, ($sp)\naddiu $sp, $sp, 4\n"  # pop $t1
-                    sub_i   = "sge $t0, $t1, $t2\n"
-                    push_i  = "sub $sp, $sp, 4\nsw $t0, ($sp)\n"    # push $t0
-                    asm.append(pop_i_b + pop_i_a + sub_i + push_i)
+                # math/logic
+                elif g[0] in bin_ops:
+                    t0 = str(); t1 = str(); t2 = str()
+                    pop_i_a = str(); pop_i_b = str();
+                    op_i = str();
+                    push_i = str()
+
+                    # dest
+                    if g[1][:-1] in locals:
+                        t0 = locals[g[1][:-1]]*4
+                        push_i  = "sw $t0, " + str(t0) + "($sp)\n"
+
+                    # src 1
+                    if g[2][:-1] in locals:
+                        t1 = locals[g[2][:-1]]*4
+                        pop_i_a = "lw $t1, " + str(t1) + "($sp)\n"
+                    elif (g[2][:-1])[0] == "$":
+                        t1 = ( (len(locals)+1)+1 + int((g[2][:-1])[1:]) )*4
+                        pop_i_a = "lw $t1, " + str(t1) + "($sp)\n"
+                    elif str(g[2][:-1]).isdigit():
+                        pop_i_b = "li $t1, " + str(g[2][:-1]) + "\n"
+
+                    # src 2
+                    if g[3] in locals:
+                        t2 = locals[g[3]]*4
+                        pop_i_b = "lw $t2, " + str(t2) + "($sp)\n"
+                    elif g[3][0] == "$":
+                        t2 = ( (len(locals)+1)+1 + int((g[3])[1:]) )*4
+                        pop_i_b = "lw $t2, " + str(t2) + "($sp)\n"
+                    elif str(g[3]).isdigit():
+                        pop_i_b = "li $t2, " + str(g[3]) + "\n"
+
+                    # add
+                    if g[0] == "+":
+                        op_i   = "add $t0, $t1, $t2\n"
+                    # sub
+                    elif g[0] == "-":
+                        op_i   = "sub $t0, $t1, $t2\n"
+                    # mul
+                    elif g[0] == "*":
+                        pass
+                    # div
+                    elif g[0] == "/":
+                        pass
+                    # mod
+                    elif g[0] == "%":
+                        pass
+                    # eq
+                    elif g[0] == "==":
+                        op_i   = "seq $t0, $t1, $t2\n"
+                    # neq
+                    elif g[0] == "!=":
+                        op_i   = "sne $t0, $t1, $t2\n"
+                    # lt
+                    elif g[0] == "<":
+                        op_i   = "slt $t0, $t1, $t2\n"
+                    # lte
+                    elif g[0] == "<=":
+                        op_i   = "sle $t0, $t1, $t2\n"
+                    # gt
+                    elif g[0] == ">":
+                        op_i   = "sgt $t0, $t1, $t2\n"
+                    # gte
+                    elif g[0] == ">=":
+                        op_i   = "sge $t0, $t1, $t2\n"
+                    asm.append(pop_i_b + pop_i_a + op_i + push_i)
+                # print int (syscall 1)
+                elif g[0] == ".>":
+                    pop_i   = "lw $a0, " + str( locals[g[1]]*4 ) + "($sp)\n"  # pop $a0
+                    print_i = "li $v0, 1\nsyscall\n"
+                    asm.append(pop_i + print_i)
                 # read int (syscall 5)
                 elif g[0] == ".<":
                     read_i = "li $v0, 5\nsyscall\n"
-                    push_i = "sub $sp, $sp, 4\nsw $v0, ($sp)\n"     # push $v0
+                    push_i = "sw $v0, " + str( locals[g[1]]*4 ) + "($sp)\n"   # push $v0
                     asm.append(read_i + push_i)
-                # print int (syscall 1)
-                elif g[0] == ".>":
-                    pop_i   = "lw $a0, ($sp)\naddiu $sp, $sp, 4\n"  # pop $a0
-                    print_i = "li $v0, 1\nsyscall\n"
-                    asm.append(pop_i + print_i)
                 # immediate int
-                elif g[0] == "=":
-                    if g[2].isdigit():
-                        imm_i  = "li $t0, " + g[2] + "\n"
-                        push_i = "sub $sp, $sp, 4\nsw $t0, ($sp)\n" # push $t0
-                        asm.append(imm_i + push_i)
+                # elif g[0] == "=":
+                #     if g[2].isdigit():
+                #         imm_i  = "li $t0, " + g[2] + "\n"
+                #         push_i = "sub $sp, $sp, 4\nsw $t0, ($sp)\n" # push $t0
+                #         asm.append(imm_i + push_i)
 
     #----------------------------------------
 
